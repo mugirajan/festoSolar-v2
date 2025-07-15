3b438efcfdcdae3b65579ca94fdbf4d71f15698f
mugilavathi <you@example.com>
2025-07-15 17:01:33 +0530

    template inserting

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HomePageComponent } from './components/home-page/home-page.component';
+
+@NgModule({
+  declarations: [
+    HomePageComponent
+  ],
+  imports: [
+    AppRoutingModule,
+  ],
+})
+export class AppModule { }
