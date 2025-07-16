373db59d7c190b5111ccee1b59ad8873e3ee8332
mugilavathi <you@example.com>
2025-07-16 11:45:41 +0530

    image bring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 import { ContactPageComponent } from './components/contact-page/contact-page.component';
 import { ServicePageComponent } from './components/service-page/service-page.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent }, 
   { path: 'About', component: AboutPageComponent },
   { path: 'Service', component: ServicePageComponent },
+  { path: 'Product', component: ProductPageComponent },
   { path: 'Contact', component: ContactPageComponent }
 ];
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 import { ContactPageComponent } from './components/contact-page/contact-page.component';
 import { ServicePageComponent } from './components/service-page/service-page.component';
+import { ProductPageComponent } from './components/product-page/product-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'About', component: AboutPageComponent },
   { path: 'Service', component: ServicePageComponent },
+  { path: 'Product', component: ProductPageComponent },
   { path: 'Contact', component: ContactPageComponent }
 ];
